3094ca967a19e83f27942bd66d955f57d4ed24f4
Yuliya <54433143+YuliyaVaravko@users.noreply.github.com>
2023-07-29 23:02:44 +0300

    Create duration.sh

---
+++ duration.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+BEGIN_TIME=$(date +%s%N)
+duration(){
+        FINISH_TIME=$(date +%s%N)
+        DURATION=$((FINISH_TIME-BEGIN_TIME))
+        echo "duration is $DURATION ms"
+        exit 0
+}
+
+while [[ $# != 0 ]]; do
+ case $1 in
+     -h)
+        cat <<-EOF
+        Program for measuring duration of command execution
+        Please use options:
+        - command which execution time you want to measure
+        - h help information
+
+         Created by Yuliya Varavko
+         yuliya.varavko@gmail.com
+EOF
+        exit 0
+        ;;
+      *)
+        echo
+        $@ && duration || echo "\nUnknow command" && exit 0
+        ;;
+ esac
+done
+
+exit 0
